e1269b37fa7085141486dcef0f151e333ff09e95
HAYGRIV VINODRAI MODHA <haygrivmodha004@gmail.com>
2024-03-13 14:15:13 +0530

    Lab Work Upload

---
+++ SEMESTER 4/WEB TECHNOLOGY LAB/Assignments/Date.js
@@ -0,0 +1,5 @@
+let currentDate = new Date();
+console.log(currentDate); // Output will be the current date and time
+
+let specificDate = new Date(2024, 2, 13, 12, 30, 0); // March 13, 2024, 12:30 PM
+console.log(specificDate);
